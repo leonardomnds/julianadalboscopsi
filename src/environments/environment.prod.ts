d1998d44ed4cb1e8e4fb5ab2a89c8c073a12a1f1
Leonardo Mendes <leonardo.mendes@insidesistemas.com.br>
2025-01-13 16:53:30 -0300

    chore: configura clarity

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   siteUrl: 'https://julianadalboscopsi.com.br',
   whatsappUrl: 'https://wa.me/+5541995753482',
-  clarityKey: '',
+  clarityKey: 'ptf18n9gg3',
   googleAdsKey: ''
 };
